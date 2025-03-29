c424d034ea6cad630ba489f78f476c19797d8c95
lifern34 <lisifern34@gmail.com>
2025-03-29 20:19:22 +0000

    Logging, etapa 2

--- client/src/client.c
+++ client/src/client.c
@@ -1,4 +1,5 @@
 #include "client.h"
+#include "stdbool.h"
 
 int main(void)
 {
@@ -18,6 +19,8 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
+
+	log_info(logger, "Hola! Soy un log");
 
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
@@ -55,6 +58,8 @@ int main(void)
 t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
+
+	nuevo_logger = log_create("tp0.log","Primer log",true,LOG_LEVEL_INFO);
 
 	return nuevo_logger;
 }
@@ -97,4 +102,5 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
 }
